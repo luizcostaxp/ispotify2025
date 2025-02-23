d6f57ea3c1f6e1bd3831c006c248b2fb528608ea
luizcostaxp <luizcostaxp@gmail.com>
2025-02-23 18:29:35 -0300

    pss final

--- iSpotify/src/components/LikedTracks/LikedTracks.jsx
+++ iSpotify/src/components/LikedTracks/LikedTracks.jsx
@@ -34,32 +34,34 @@ export default function LikedTracks({ userId }) {
 	};
 
 	return (
-		<div className={styles.trackContainer}>
-			{likedTracks.map((track, index) => (
-			<div className={styles.trackInfo} key={track.id}>
-				<div className={styles.trackNameAndArtistAndIndex}>
-					<div className={styles.trackIndex}>
-						<span>{index + 1}</span>
+		<div className={styles.mainContainer}>
+			<div className={styles.trackContainer}>
+				{likedTracks.map((track, index) => (
+				<div className={styles.trackInfo} key={track.id}>
+					<div className={styles.trackNameAndArtistAndIndex}>
+						<div className={styles.trackIndex}>
+							<span>{index + 1}</span>
+						</div>
+						<div className={styles.trackNameAndArtistName}>
+							<p className={styles.trackName}>{track.title}</p>
+							<p className={styles.artistName}>
+								{track.artistName}
+							</p>
+						</div>
 					</div>
-					<div className={styles.trackNameAndArtistName}>
-						<p className={styles.trackName}>{track.title}</p>
-						<p className={styles.artistName}>
-							{track.artistName}
-						</p>
+					<div className={styles.albumName}>
+						{track.genre}
+					</div>
+					<div>
+						<DeleteIcon
+							className={styles.deleteIcon}
+							style={{ fontSize: "3.5rem", marginTop: "1.5rem" }}
+							onClick={() => handleUnlike(track.id)}
+						/>
 					</div>
 				</div>
-				<div className={styles.albumName}>
-					{track.genre}
-				</div>
-				<div>
-					<DeleteIcon
-						className={styles.deleteIcon}
-						style={{ fontSize: "3.5rem", marginTop: "1.5rem" }}
-						onClick={() => handleUnlike(track.id)}
-					/>
-				</div>
+			))}
 			</div>
-		))}
 		</div>
 	);
 }
--- iSpotify/src/pages/LikedMusics/LikedMusics.jsx
+++ iSpotify/src/pages/LikedMusics/LikedMusics.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import LikedTracks from "../../components/LikedTracks/LikedTracks";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import styles from "./LikedMusics.module.css";
+import img from "../../assets/likedsongs.png"; // Altere para o nome correto da sua imagem
 
 export default function LikedMusics() {
     const { userId } = useParams();
@@ -12,22 +13,39 @@ export default function LikedMusics() {
 
     return (
         <div className={styles.userLikedMusicsPage}>
-            <Sidebar />
-            <div className={styles.tracksContainer}>
-                <div className={styles.header}>
-                    <p># TÍTULO</p>
-                    <p>Gênero</p>
-                    <AccessTimeIcon
-                        style={{
-                            fontSize: "28px",
-                            color: "white",
-                            marginTop: "2rem",
-                        }}
-                    />
+            <Sidebar></Sidebar>
+            <div className={styles.mainContainer}>
+                <div className={styles.artistInfy}>                    
+                    <div className={styles.artistInfo}>
+                        <img 
+                            src={img} 
+                            alt="Imagem de músicas curtidas" 
+                            className={styles.artistImage} 
+                        />
+                        <div className={styles.artistDetails}>
+                            <p className={styles.artistLabel}>PLAYLIST</p>
+                            <p className={styles.artistName}>Músicas Curtidas</p>
+                        </div>
+                    </div>
+                   
+                
                 </div>
-                <hr className={styles.separator} />
-                <div className={styles.tracks}>
-                    <LikedTracks userId={userIdNumber} />
+                <div className={styles.tracksContainer}>
+                    <div className={styles.header}>
+                        <p># TÍTULO</p>
+                        <p>Gênero</p>
+                        <AccessTimeIcon
+                            style={{
+                                fontSize: "28px",
+                                color: "white",
+                                marginTop: "2rem",
+                            }}
+                        />
+                    </div>
+                    <hr className={styles.separator} />
+                    <div className={styles.tracks}>
+                        <LikedTracks userId={userIdNumber} />
+                    </div>
                 </div>
             </div>
         </div>
