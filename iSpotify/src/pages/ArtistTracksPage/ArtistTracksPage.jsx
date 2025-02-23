9901c5f39e8f0a95b461f84db78894578a93e983
luizcostaxp <luizcostaxp@gmail.com>
2025-02-23 17:29:35 -0300

    crucial

--- iSpotify/src/pages/ArtistTracksPage/ArtistTracksPage.jsx
+++ iSpotify/src/pages/ArtistTracksPage/ArtistTracksPage.jsx
@@ -1,35 +1,84 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Tracks from "../../components/Tracks/Tracks";
-
+import { getArtist } from "../../spotify";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 import styles from "./ArtistTracksPage.module.css";
 
 export default function ArtistTracksPage() {
-	const { artistId } = useParams();
+    const { artistId } = useParams();
+    const [artist, setArtist] = useState(null);
 
-	return (
-		<div className={styles.artistTracksPage}>
-			<Sidebar></Sidebar>
-			<div className={styles.tracksContainer}>
-				<div className={styles.header}>
-					<p>#TÍTULO</p>
-					<p>GÊNERO</p>
-					<AccessTimeIcon
-						style={{
-							fontSize: "3.5rem",
-							color: "white",
-							marginTop: "2rem",
-						}}
-					/>
+    useEffect(() => {
+        const fetchArtist = async () => {
+            try {
+                const artistData = await getArtist(artistId);
+                setArtist(artistData);
+            } catch (error) {
+                console.error("Erro ao buscar detalhes do artista:", error);
+            }
+        };
+
+        fetchArtist();
+    }, [artistId]);
+
+    return (
+        <div className={styles.artistTracksPage}>
+            <Sidebar></Sidebar>
+			<div className={styles.mainContainer}>
+				<div className={styles.artistInfy}>
+				{artist && (
+                    <div className={styles.artistInfo}>
+                        <img 
+                            className={styles.artistImage} 
+                            src={artist.image} 
+                            alt={artist.name} 
+                        />
+                        <div className={styles.artistDetails}>
+                            <p className={styles.artistLabel}>ARTISTA</p>
+                            <p className={styles.artistName}>{artist.name}</p>
+                        </div>
+                    </div>
+                )}
+
 				</div>
-				<hr className={styles.separator} />
-				<div className={styles.tracks}>
-					<Tracks artistId={parseInt(artistId)}></Tracks>
+				<div className={styles.buttons}>
+					<div className={styles.playButton}>
+        				<span className="material-icons" style={{ fontSize: '30px', color: 'black' }}>
+            				play_arrow
+        				</span>
+    				</div>
+					<span className="material-icons" style={{ fontSize: '30px', color: 'white' }}>
+						favorite_border
+					</span>
+					<span className="material-icons" style={{ fontSize: '30px', color: 'white' }}>
+						download
+					</span>
+					<span className="material-icons" style={{ fontSize: '30px', color: 'white' }}>
+						more_vert
+					</span>
+				</div>
+				<div className={styles.tracksContainer}>
+					<div className={styles.header}>
+						<p># TÍTULO</p>
+						<p>GÊNERO</p>
+						<AccessTimeIcon
+							style={{
+								fontSize: "3.5rem",
+								color: "white",
+								marginTop: "2rem",
+							}}
+						/>
+					</div>
+					<hr className={styles.separator} />
+					<div className={styles.tracks}>
+						<Tracks artistId={parseInt(artistId)} />
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+        </div>
+    );
 }
